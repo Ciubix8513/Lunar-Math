f00107b246d2cae56eea6cbfb892e35e773ef2ea
Ciubix8513 <57328501+Ciubix8513@users.noreply.github.com>
2022-02-16 00:17:24 +0300

    Finished tests for vec2, reworked vec2, added [], = operators to vec2

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -3,14 +3,26 @@
 
 using namespace LunarMath;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace Microsoft
+{
+	namespace VisualStudio
+	{
+		namespace CppUnitTestFramework
+		{
+			template<> static std::wstring ToString<Vector2>(const class Vector2& v)
+			{
+				auto s = v.ToString();
+				return std::wstring(s.begin(), s.end());
+			}
+		}
+	}
+}
 
 namespace Tests
 {
 	TEST_CLASS(Vector2Tests)
 	{
 	public:
-
-		
 		TEST_METHOD(ConstructorTest)
 		{
 			auto v = Vector2();
@@ -27,9 +39,7 @@ namespace Tests
 		{
 			Vector2 v, v1;
 			v = v1 = Vector2(1, 0);
-			v == v1;
 			Assert::AreEqual(v1, v);
-
 		};
 
 		TEST_METHOD(LengthTests)
@@ -43,26 +53,73 @@ namespace Tests
 		TEST_METHOD(NormalizeTests)
 		{
 			auto v = Vector2(10, 0);
-			
+			Vector2 v1 = Vector2::Normalized(v);
+			Assert::AreEqual(v1, Vector2(1, 0));
+			v1 = v.Normalized();
+			Assert::AreEqual(v1, Vector2(1, 0));
+			v1 = v.Normalize();
+			Assert::AreEqual(v, Vector2(1, 0));
+			Assert::AreEqual(v1, Vector2(1, 0));
+			v = Vector2(2, 0);
+			v1 = Vector2::Normalize(v);
+			Assert::AreEqual(v, Vector2(1, 0));
+			Assert::AreEqual(v1, Vector2(1, 0));
+		};
 
-		};	
+		TEST_METHOD(DotProductTest)
+		{
+			Assert::AreEqual(vec2::Dot(vec2(1, 0), vec2(0, 1)), 0.0f);
+			Assert::AreEqual(vec2::Dot(vec2(1, 0), vec2(1, 0)), 1.0f);
+		};
 
 		TEST_METHOD(EqualsTest)
 		{
-			Vector2 v = Vector2(1, 1);			
+			Vector2 v = Vector2(1, 1);
 			Assert::IsTrue(v == v);
 		};
-		
 
+		TEST_METHOD(FloatOperatorsTest)
+		{
+			auto v = Vector2(2, 0);
+			Assert::AreEqual(Vector2(1, 0), v / 2);
+			Assert::AreEqual(Vector2(1, 0), v * .5f);
+			auto v1 = v /= 2.0f;
+			Assert::AreEqual(Vector2(1, 0), v1);
+			Assert::AreEqual(Vector2(1, 0), v);
 
+			v1 = v *= 2.0f;
+			Assert::AreEqual(Vector2(2, 0), v1);
+			Assert::AreEqual(Vector2(2, 0), v);
+		};
+		TEST_METHOD(VectorOperatorsTest)
+		{
+			auto v = Vector2(2, 0);
+			Assert::AreEqual(Vector2(1, 0), v - Vector2(1, 0));
+			Assert::AreEqual(Vector2(1, 0), v + Vector2(-1, 0));
+			auto v1 = v -= Vector2(1, 0);
+			Assert::AreEqual(Vector2(1, 0), v1);
+			Assert::AreEqual(Vector2(1, 0), v);
+
+			v1 = v += Vector2(1, 0);
+			Assert::AreEqual(Vector2(2, 0), v1);
+			Assert::AreEqual(Vector2(2, 0), v);
+		}
+
+		TEST_METHOD(VectorSubscriptTest)
+		{
+			auto v = Vector2(1, 2);
+			Assert::AreEqual(1.0f, v[0]);
+			Assert::AreEqual(2.0f, v[1]);
+
+			v[0] += 1;
+			Assert::AreEqual(2.0f, v[0]);
+		}
 	};
 	/*TEST_CLASS(Vector3Tests)
 	{
 	public:
 		TEST_METHOD(EqualsTest)
 		{
-			
 		}
 	};*/
-
 }
--- Vector2.h
+++ Vector2.h
@@ -2,17 +2,25 @@
 #ifndef _VECTOR2_H_
 #define _VECTOR2_H_
 #include <math.h>
+#include <sstream>
+#include <string>
 #include <exception>
-#include <DirectXMath.h>
+
 //using namespace std;
+#define Dot DotProduct 
+#define norm Nomalize
+#define normd Nomalized 
+#define vec2 Vector2 
 
 namespace LunarMath
 {
 #define Deg2Rad  0.0174532925f
-#define PI (double)3.1415926535897932384626433832795f	
+#define PI (double) 3.1415926535897932384626433832795f	
 	class  Vector2
 	{
 	public:
+		std::string ToString() const;
+
 #pragma region VectorCreation + Consts
 		Vector2();
 		Vector2(const float& f);
@@ -25,27 +33,32 @@ namespace LunarMath
 #pragma endregion
 
 #pragma region Functions
-		float Length();
+		float Length() const;
 		static float Length(const Vector2& v);
-		Vector2 Normalized();
-		static void Normalize(Vector2& v);
-		void Normalize();
-		static float DotProduct(const Vector2 a, Vector2 b);
+
+		Vector2 Normalized() const;
+		static Vector2 Normalized(const Vector2& v);
+
+		static  Vector2& Normalize(Vector2& v);
+		Vector2& Normalize();
+
+		static float DotProduct(const Vector2& a,const Vector2& b);
 
 #pragma endregion
 
 #pragma region operators
-		Vector2 operator=(const Vector2& v);
-		Vector2 operator+(Vector2 v);
-		Vector2 operator-(Vector2 v);
-		Vector2 operator*(float c);
-		Vector2 operator/(float c);
-		void operator+=(Vector2 v);
-		void operator-=(Vector2 v);
-		void operator*=(float c);
-		void operator/=(float c);
-		bool operator==(const Vector2& v);
-		
+		float operator[](const int& i) const;
+		float& operator[](const int& i) ;
+		Vector2& operator=(const Vector2& v);
+		Vector2 operator+(const Vector2& v) const;
+		Vector2 operator-(const Vector2& v) const;
+		Vector2 operator*(const float& c) const;
+		Vector2 operator/(const float& c) const;
+		Vector2& operator+=(const Vector2& v);
+		Vector2& operator-=(const Vector2& v);
+		Vector2& operator*=(const float& c);
+		Vector2& operator/=(const float& c);
+		bool operator==(const Vector2& v) const;
 #pragma endregion
 		float x, y;
 	};
